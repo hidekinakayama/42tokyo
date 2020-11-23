c7feaff30457e40ff3d5c4ef8af53e89509bd473
中山英樹 <nakayamahideki@nakayamahidekinoMacBook-Pro.local>
2020-11-24 02:14:56 +0900

    完成版

--- git_commit.sh
+++ git_commit.sh
@@ -1,1 +1,12 @@
-git log
+#!/usr/bin/bash
+
+echo '' > commitID.txt
+echo '' > commitID2.txt
+echo '' > commitID3.txt
+git log > commitID.txt
+grep -w 'commit' commitID.txt > commitID2.txt
+sed -e 's/commit//g' commitID2.txt > commitID3.txt
+sed '1,5!d' commitID3.txt 
+rm commitID.txt
+rm commitID2.txt
+rm commitID3.txt
